8246e86f018a160cc60b2b4f3d98ac6153e76191
Walid Moheyeldin <wmoheyeldin@netways.com>
2023-06-21 15:36:38 +0300

    Add project files.

---
+++ BlazorDynamicJsComponent/Server/Controllers/HtmlComponentsController.cs
@@ -0,0 +1,45 @@
+﻿using BlazorDynamicJsComponent.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorDynamicJsComponent.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HtmlComponentsController : ControllerBase
+    {
+        private static readonly HtmlComponent[] Components = new HtmlComponent[]
+        {
+            new HtmlComponent{
+                Element = HtmlElement.button,
+                Content = "Click me",
+                Events = new Dictionary<HtmlElementEvent, string>()
+                {
+                    { HtmlElementEvent.onClick, @"alert('Hi from click event');" },
+                    { HtmlElementEvent.onMouseOver, "console.log('Hi from mouse over event');" }
+                }
+            },
+            new HtmlComponent{
+                Element = HtmlElement.checkbox,
+                Content = "Is True",
+                Events = new Dictionary<HtmlElementEvent, string>()
+                {
+                    { HtmlElementEvent.onChange, "var isTrue = this.checked; alert(`My value changed from ${!isTrue} to ${isTrue}`);" }
+                }
+            }
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public HtmlComponentsController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<HtmlComponent> Get()
+        {
+            return Components;
+        }
+    }
+}
---
+++ BlazorDynamicJsComponent/Shared/HtmlElement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorDynamicJsComponent.Shared
+{
+    public enum HtmlElement
+    {
+        form, button, checkbox, radio
+    }
+
+    public enum HtmlElementEvent
+    {
+        onClick, onDblClick, onMouseDown, onMouseUp, onChange, onKeyDown, onKeyUp, onMouseOver, onChecked, onUnchecked
+    }
+
+    public class HtmlComponent
+    {
+        public HtmlElement Element { get; set; }
+        public string Content { get; set; }
+        public Dictionary<HtmlElementEvent, string> Events { get; set; }
+    }
+}
